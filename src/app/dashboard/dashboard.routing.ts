29a2aaf832cc8c98f2c650ef4c49b382deba3ac9
Bryan Arriaza <bryan.arriaza.g1993@gmail.com>
2019-02-08 11:52:57 -0600

    Template base listo

---
+++ src/app/dashboard/dashboard.routing.ts
@@ -0,0 +1,18 @@
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+
+import { EstadisticaComponent } from '../ingreso-egreso/estadistica/estadistica.component';
+import { IngresoEgresoComponent } from '../ingreso-egreso/ingreso-egreso.component';
+import { DetalleComponent } from '../ingreso-egreso/detalle/detalle.component';
+
+export const dashboardRoutes: Routes = [
+  { path: '', component: EstadisticaComponent },
+  { path: 'ingreso-egreso', component: IngresoEgresoComponent },
+  { path: 'detalle', component: DetalleComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(dashboardRoutes)],
+  exports: [RouterModule]
+})
+export class DashboardRoutingModule {}
