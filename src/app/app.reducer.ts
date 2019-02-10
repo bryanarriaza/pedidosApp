cb72cb5608b3f20c504a933ba490ba87f2e4612b
Bryan Arriaza <bryan.arriaza.g1993@gmail.com>
2019-02-09 22:30:09 -0600

    Sección 9 - Obtener el UID de nuestros elementos de Firebase Control de datos como lista desde firebase para almacenarlo en el Store ngrx

--- src/app/app.reducer.ts
+++ src/app/app.reducer.ts
@@ -1,13 +1,16 @@
 import { ActionReducerMap } from '@ngrx/store';
 import * as fromUILoad from './shared/ui.reducer';
 import * as fromAuth from './auth/auth.reducer';
+import * as fromIngresoEgreso from './ingreso-egreso/ingreso-egreso.reducer';
 
 export interface AppState {
-    uiLoad: fromUILoad.State;
-    auth: fromAuth.AuthState;
+  uiLoad: fromUILoad.State;
+  auth: fromAuth.AuthState;
+  ingresoEgreso: fromIngresoEgreso.IngresoEgresoState;
 }
 
 export const appReducers: ActionReducerMap<AppState> = {
-    uiLoad: fromUILoad.uiReducer,
-    auth: fromAuth.authReducer
+  uiLoad: fromUILoad.uiReducer,
+  auth: fromAuth.authReducer,
+  ingresoEgreso: fromIngresoEgreso.ingresoEgresoReducer
 };
--- src/app/ingreso-egreso/ingreso-egreso.service.ts
+++ src/app/ingreso-egreso/ingreso-egreso.service.ts
@@ -1,18 +1,57 @@
 import { Injectable } from '@angular/core';
 
+// NgRx
+import { Store } from '@ngrx/store';
+import { AppState } from '../app.reducer';
+import { SetItemsAction } from './ingreso-egreso.actions';
+
 // Firebase
 import { AngularFirestore } from '@angular/fire/firestore';
 import { IngresoEgreso } from './ingreso-egreso.model';
 import { AuthService } from '../auth/auth.service';
+import { filter, map } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class IngresoEgresoService {
+  ingresoEgresoListerSubscription: Subscription = new Subscription();
+  ingresoEgresoItemsSubscription: Subscription = new Subscription();
+
   constructor(
     private afDB: AngularFirestore,
-    public authService: AuthService
+    public authService: AuthService,
+    private store: Store<AppState>
   ) {}
+
+  initIngresoEgresoListener() {
+    this.ingresoEgresoListerSubscription = this.store
+      .select('auth')
+      .pipe(filter(auth => auth.user != null))
+      .subscribe(auth => {
+        this.ingresoEgresoItems(auth.user.uid);
+      });
+  }
+
+  private ingresoEgresoItems(uid: string) {
+    this.ingresoEgresoItemsSubscription = this.afDB
+      .collection(`${uid}/ingresos-egresos/items`)
+      .snapshotChanges()
+      .pipe(
+        map(docData => {
+          return docData.map(doc => {
+            return {
+              uid: doc.payload.doc.id,
+              ...doc.payload.doc.data()
+            };
+          });
+        })
+      )
+      .subscribe((coleccion: any) => {
+        this.store.dispatch(new SetItemsAction(coleccion));
+      });
+  }
 
   crearIngresoEgreso(ingresoEgreso: IngresoEgreso) {
     const user = this.authService.getUsuario();
@@ -22,5 +61,10 @@ export class IngresoEgresoService {
       .add({
         ...ingresoEgreso
       });
+  }
+
+  cancelarSubscriptions() {
+    this.ingresoEgresoListerSubscription.unsubscribe();
+    this.ingresoEgresoItemsSubscription.unsubscribe();
   }
 }
